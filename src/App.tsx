f694611c82b314c72bd49100c4abbc1f8c1d4e02
Henrique Moreira <riqimoreiraa@gmail.com>
2022-06-10 16:25:23 -0300

    weather infos finalizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useSyncExternalStore } from 'react';
 import axios from 'axios';
 import './App.css';
+import { Cloud, FeelsLike } from './svgs'
 
 type coordenates = {
   lon: string;
@@ -12,6 +13,7 @@ const App = () => {
   const [coords, setCoords] = useState<coordenates[]>()
   const [currentLocation, setCurrentLocation] = useState(false)
   const [localName, setLocaName] = useState('')
+  const [city, setCity] = useState()
  
   const getCoords = () => {
     axios.get(`https://nominatim.openstreetmap.org/search?format=json&limit=1&q=${localName}`)
@@ -19,12 +21,17 @@ const App = () => {
       setCoords(res.data)
     })
   }
-  
+ 
   const getWeather = (lat: number, lon: number) => {
-    axios.get(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${lat}&lon=${lon}`)
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=daily,hourly,minutely,lat,lon,timezone,timezone_offset&appid=1c2a982b34c4acb2a57bbd523ff8b146
+    `)
     .then(res => {
       setWeatherData(res.data)
     })
+    axios.get(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${lat}&lon=${lon}`)
+     .then(res => {
+       setCity(res.data)
+     })
   }
 
   useEffect(() => {
@@ -42,20 +49,48 @@ const App = () => {
   }, [coords !== undefined])
 
   return (
-    <div>
-      <input onChange={(e) => setLocaName(e.target.value)} type="text" />
-      <button onClick={getCoords}>search</button>
+    <div className='container'>
+      <div>
+        <input onChange={(e) => setLocaName(e.target.value)} type="text" />
+        <button onClick={getCoords}>search</button>
+      </div>
 
-      {weatherData === undefined ? (
+      {city === undefined || weatherData === undefined ? (
             <div>carregando...</div>
             ) : (
-              <div>
-                <h2>{weatherData['name']}, {weatherData['sys']['country']}</h2>
-                <h1>{weatherData['main']['temp']}°</h1>
-                <h1>{weatherData['weather'][0]['main']}</h1>
-                <h2>{weatherData['weather'][0]['description']}</h2>
-                <img  src={weatherData['weather'][0]['icon']} alt={weatherData['weather'][0]['description']} />
+              <div className='weatherInfo'>
+                <h2>{city['name']}, {city['sys']['country']}</h2>
 
+                <div className='temp'>
+                  <img  src={`http://openweathermap.org/img/wn/${weatherData['current']['weather'][0]['icon']}@2x.png`}/>
+                  <h1>{parseInt(weatherData['current']['temp'])}°</h1>
+                </div>
+
+                <div className='description'>
+                  <Cloud />
+                  <h3 className='descH3'>{weatherData['current']['weather'][0]['main']} | {weatherData['current']['weather'][0]['description']}</h3>
+                  <FeelsLike />
+                  <h3 className='feelsLikeH3'> Feels like {parseInt(weatherData['current']['feels_like'])}°</h3>
+                </div>
+
+                <div className='wind'>
+                  <div className='colorDIVwind'></div> 
+                  <div className='windKMH'>
+                    <p>WIND </p>
+                    <p>{weatherData['current']['wind_speed']} mt/s</p>
+                  </div>
+                </div>
+                <div className='wind'>
+                  <div className='colorDIVumidity'></div> 
+                  <div className='windKMH'>
+                    <p>HUMIDITY</p>
+                    <p>{weatherData['current']['humidity']}%</p>
+                  </div>
+                </div>
+
+                <div>
+
+                </div>
               </div>
             )
           }
---
+++ src/svgs.tsx
@@ -0,0 +1,15 @@
+export const Cloud = () => {
+    return (
+        <svg width="28" height="28" fill="none" stroke="#454545" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18.75 11.25C18.333 7.053 15.422 4.5 12 4.5 8.766 4.5 6.683 6.76 6 9 3.187 9.281.75 11.043.75 14.25c0 3.094 2.531 5.25 5.625 5.25h12.188c2.578 0 4.687-1.286 4.687-4.125 0-2.804-2.484-4.02-4.5-4.125Z"></path>
+        </svg>
+    )
+}
+export const FeelsLike = () => {
+    return (
+        <svg width="25" height="25" fill="#454545" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <path d="M15 13.652V3.75a3 3 0 0 0-6 0v9.902A5.314 5.314 0 0 0 6.75 18a5.25 5.25 0 0 0 10.5 0A5.316 5.316 0 0 0 15 13.652Zm-3 6.598a2.25 2.25 0 0 1-.75-4.372V4.5h1.5v11.378A2.25 2.25 0 0 1 12 20.25Z"></path>
+</svg>
+    )
+}
+
